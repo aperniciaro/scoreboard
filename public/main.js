14a5cb1aab4342db72360aa45270a7fea81b47e1
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-04 12:47:02 -0500

    Set up page

--- public/main.js
+++ public/main.js
@@ -1,7 +1,45 @@
+let team1Counter = 0
+
+let team2Counter = 0
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  document.querySelectorAll('p').textContent = '0'
+}
+
+const addTeam1 = () => {
+  team1Counter++
+  document.querySelector('team1Score').textContent = team1Counter
+}
+
+const addTeam2 = () => {
+  team2Counter++
+  document.querySelector('team2Score').textContent = team2Counter
+}
+
+const subtractTeam1 = () => {
+  team1Counter--
+  document.querySelector('team1Score').textContent = team1Counter
+}
+
+const subtractTeam2 = () => {
+  team2Counter--
+  document.querySelector('team2score').textContent = team2Counter
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.add-one-button')
+  .addEventListener('click', increaseCounter)
+
+document
+  .querySelector('.add-one-button')
+  .addEventListener('click', increaseCounter)
+
+document
+  .querySelector('.add-one-button')
+  .addEventListener('click', increaseCounter)
+
+document
+  .querySelector('.add-one-button')
+  .addEventListener('click', increaseCounter)
